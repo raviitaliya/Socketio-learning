d5d34343b9523ad0fbca0dbc88bca74b00ec0bd5
ravi italiya <italiyaravi704@gmail.com>
2024-04-27 14:33:46 +0530

    secket server setup

--- server/app.js
+++ server/app.js
@@ -1,13 +1,22 @@
 import express from "express";
+import { Server } from "socket.io";
+import {createServer} from "http";
 
 const port = 3000;
 
 const app = express();
+const server = createServer(app);
 
+const io = new Server(server);
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
+
+io.on("connection",(Socket) =>{
+    console.log("user created");
+    console.log("id", Socket.id);
+})
 
 app.listen(port, () => {
   console.log(`server is runing on ${port}`);
